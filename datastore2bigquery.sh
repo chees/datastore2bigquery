4851a0889057e87b16627da8546960f0f02e9ac3
Christiaan Hees <mail@chees.info>
2019-05-22 16:30:26 +0200

    Fixed --project param and concurrent gsutil rm

--- datastore2bigquery.sh
+++ datastore2bigquery.sh
@@ -21,15 +21,15 @@ if [ -n "${3:-}" ]; then BQDATASET=$3; else BQDATASET=datastore; fi
 GCSPATH=gs://$PROJECT.appspot.com/datastore-$KIND-`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 
 echo -e "${GREEN}Starting datastore export...$NC"
-gcloud beta datastore export --kinds="$KIND" --project=$PROJECT $GCSPATH
+gcloud datastore export --kinds="$KIND" --project=$PROJECT $GCSPATH
 
 echo -e "${GREEN}Creating BigQuery dataset...$NC"
 bq mk -f $PROJECT:$BQDATASET
 
 echo -e "${GREEN}Loading export into BigQuery...$NC"
-bq load --project $PROJECT --source_format=DATASTORE_BACKUP $PROJECT:$BQDATASET.${KIND}_`date -u +"%Y%m%dT%H%M%SZ"` $GCSPATH/all_namespaces/kind_$KIND/all_namespaces_kind_$KIND.export_metadata
+bq load --project_id $PROJECT --source_format=DATASTORE_BACKUP $PROJECT:$BQDATASET.${KIND}_`date -u +"%Y%m%dT%H%M%SZ"` $GCSPATH/all_namespaces/kind_$KIND/all_namespaces_kind_$KIND.export_metadata
 
 echo -e "${GREEN}Deleting backup files from Cloud Storage...$NC"
-gsutil rm -r $GCSPATH
+gsutil -m rm -r $GCSPATH
 
 echo -e "${GREEN}Done$NC"
